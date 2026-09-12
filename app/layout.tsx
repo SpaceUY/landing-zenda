03a26ec27c5e1fc1ddf785e367aebb434772fc3b
IvanBarbarosch <ivan@0xfutbol.com>
2026-09-12 12:46:35 -0300

    feat: add Meta Pixel for PageView tracking and Meta Ads attribution

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { OPENPANEL_API_URL, SITE_URL } from "@/lib/constants";
 import "./globals.css";
 
 const GA_MEASUREMENT_ID = "G-0ZYLHML5PY";
+const META_PIXEL_ID = "1149512157531866";
 
 const sora = Sora({
   variable: "--font-sora",
@@ -106,6 +107,34 @@ export default function RootLayout({
         trackOutgoingLinks
         trackAttributes
       />
+      {META_PIXEL_ID && (
+        <>
+          <Script id="meta-pixel-init" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${META_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              height="1"
+              width="1"
+              alt=""
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            />
+          </noscript>
+        </>
+      )}
     </html>
   );
 }
